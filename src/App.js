5286a960bf5bf6ecd5f951226668564bbb276dfb
ram5353 <ramkiran5353@gmail.com>
2022-09-25 03:11:31 -0400

    added home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/pages/HomePage/Home';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={ <Home />} />
       </Routes>
     </div>
   );
